b44d2e2ae6a024c72e47c1a9cfbeb9fd977d23eb
Samuel <lingofthesam@gmail.com>
2020-10-02 21:39:14 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// Infinite Scroll
+// Build an image viewer with infinite scrolling, similar to Pinterest.
+
+// For the purpose of this challenge, images can be random avatars, generated using this free API, e.g. http://api.adorable.io/avatars/32131231 (the actual avatar ID can be any random integer).
+
+// When you feel ready, you can validate your solution using any collaborative HTML/CSS renderer, such as this one.
+
+const axios = require('axios');
+
+const randomInteger = Math.floor(Math.random() * 10000000);
+
+const url = `http://api.adorable.io/avatars/${randomInteger}`
+axios.get(url).then(res => {
+    // res.json(apiRes.data)
+
+    console.log(res)
+})
