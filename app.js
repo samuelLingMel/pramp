eda01a8f8f89c98df04b2528c40e7f383f3f5361
Samuel <lingofthesam@gmail.com>
2020-10-04 13:32:45 +1100

    the problem was simple, I overthought it

--- app.js
+++ app.js
@@ -6,13 +6,15 @@
 
 // When you feel ready, you can validate your solution using any collaborative HTML/CSS renderer, such as this one.
 
-const axios = require('axios');
+// const axios = require('axios');
 
-const randomInteger = Math.floor(Math.random() * 10000000);
+// const randomInteger = Math.floor(Math.random() * 10000000);
 
-const url = `http://api.adorable.io/avatars/${randomInteger}`
-axios.get(url).then(res => {
-    // res.json(apiRes.data)
+// const url = `http://api.adorable.io/avatars/${randomInteger}`
+// axios.get(url).then(res => {
+//     // res.json(apiRes.data)
 
-    console.log(res)
-})
+//     console.log(res)
+// })
+
+
